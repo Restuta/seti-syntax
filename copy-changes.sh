772d778b78262b62e32ea48a768e968ca6e3ff04
Restuta <restuta8@gmail.com>
2015-10-15 10:47:52 -0700

    updated jade syntax and added bash script to copy changes from atom packages folder

---
+++ copy-changes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#copies theme files from atom package folder
+ATOM_PACKAGES_DIR="/Users/Restuta/.atom/packages/seti-syntax"
+
+SAMPLE_FILES_DIR="$ATOM_PACKAGES_DIR/sample-files"
+STYLES_DIR="$ATOM_PACKAGES_DIR/styles"
+INDEX_LESS_FILE="$ATOM_PACKAGES_DIR/index.less"
+
+DESTINATION="."
+
+echo "cp -r "$SAMPLE_FILES_DIR" "$DESTINATION""
+cp -r "$SAMPLE_FILES_DIR" "$DESTINATION"
+cp -r $STYLES_DIR $DESTINATION
+cp -r $INDEX_LESS_FILE $DESTINATION
+ 
